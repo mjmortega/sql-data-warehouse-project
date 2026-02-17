958dda7a576f06ae8e170adff607dc71c1b7fef5
Shota-kun <markjamesortega4728@gmail.com>
2026-02-17 22:52:56 +0800

    update

--- scripts/silver/quality_check.sql
+++ scripts/silver/quality_check.sql
@@ -35,3 +35,17 @@ WHERE sls_order_dt <= 0
 -- Check for Invalid Date Orders
 SELECT * FROM bronze.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative.
+
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
