8ffb1e631122840dd6032f0a4afc47e2d8c8e867
Shota-kun <markjamesortega4728@gmail.com>
2026-03-06 16:56:49 +0800

    product

---
+++ scripts/gold/clean product.sql
@@ -0,0 +1,15 @@
+SELECT
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL --Filter out all historical data
